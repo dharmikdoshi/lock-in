941b430ef9a3e474aee82b94a232046542385c56
Dharmik <dharmikdoshi7@gmail.com>
2024-04-25 23:58:10 +0530

    lock in

---
+++ src/main/java/com/lockin/lockedinstream/LockedInStreamApplication.java
@@ -0,0 +1,13 @@
+package com.lockin.lockedinstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockedInStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockedInStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lockin/lockedinstream/LockedInStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lockin.lockedinstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockedInStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
